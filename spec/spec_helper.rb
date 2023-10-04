5d5d421f3c8b8e462a443430818de3cad087fc13
denysnando <denysnando@gmail.com>
2023-10-04 10:18:19 -0300

    setup rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'quake_log'
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
